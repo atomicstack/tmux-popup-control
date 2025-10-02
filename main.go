413a1da481fe755c7d742571a33b9605c3769350
atomicstack <27742+atomicstack@users.noreply.github.com>
2025-10-02 11:40:51 +0200

    Add minimal Go main.go using Bubbletea

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbletea"
+)
+
+// Placeholder for tmux control socket integration
+func tmuxControlSocket() {
+	// TODO: Implement tmux control socket integration
+}
+
+func main() {
+	fmt.Println("Hello, Bubbletea!")
+	// Start the Bubbletea program
+}
